ad5bd70492a26b48bfb5b19ea3116e1e47aa3c72
jashenawy <100783932+jashenawy@users.noreply.github.com>
2022-03-19 16:29:59 +0300

    DONE

--- BMI Calculator/script.js
+++ BMI Calculator/script.js
@@ -1,1 +1,27 @@
 // هنا سوف تنشئ دالة calculate 
+function calculate() {
+let height = document.getElementById("height").value;
+let weight = document.getElementById("weight").value;
+
+// if(height =="" || weight == ""){
+//     alert("قم بالتاكد من تعبئه جميع بياناتك المطلوبه");
+//     return;
+// }
+
+let BMI = weight / (height * height);
+let status = "";
+
+if (BMI  < 18.5) {
+    status ="تعاني من نقص في الوزن";
+} else if (BMI < 25) {
+    status = "وزنك صحي";
+} else if (BMI < 30) {
+    status = "تعاني من زياده في الوزن";
+} else {
+    status = "تعاني من السمنه المفرطه";
+}
+
+BMI = BMI.toFixed(2);
+document.getElementById("result").innerHTML =BMI;
+document.getElementsByClassName("comment")[0].innerHTML = status;
+}
---
+++ Exercise 1/ex1.js
@@ -0,0 +1,18 @@
+function travel(x ="زحل"){
+    console.log("اود السفر الى كوكب زحل " + x);
+} 
+
+let planet = prompt("اين سوف تذهب؟");
+
+travel(planet);
+
+
+
+
+(function travel (x = "زمرده") {
+    console.log(" اود السفر" + x);
+}) (planet);
+
+// (function travel (x = "زمرده") {
+//     console.log(" اود السفر" + x);
+// }) ();
